1e4294c3e578247be95b0382cd39b84556720077
Hamdi <hamdi.oezsu@gmail.com>
2020-10-07 16:47:14 +0200

    commit

--- src/main/java/testdb/App.java
+++ src/main/java/testdb/App.java
@@ -13,14 +13,53 @@ public class App
 {
     public static void main( String[] args )
     {
-        try{
+
+        showInvoices();
+
+       /* try{
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/jdbc","root","");
             Statement stmt=con.createStatement();
-            ResultSet rs=stmt.executeQuery("select Marke,Reichweite from tabelle");
+            ResultSet rs=stmt.executeQuery("select * from invoice");
             while(rs.next())
-                System.out.println(rs.getString("Marke")+"  "+rs.getInt("Reichweite"));
+                System.out.println(rs.getInt(1)+"  "+rs.getString(3));
             con.close();
-        }catch(Exception e){ System.out.println(e);}
+        }catch(Exception e){ System.out.println(e);}*/
     }
+
+    static Connection con;
+    static Statement stmt;
+    public static void conDatabase() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            App.con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/jdbc", "root", "");
+            App.stmt = App.con.createStatement();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+    public static void showInvoices(){
+            conDatabase();
+            try{
+                ResultSet rs= App.stmt.executeQuery("select * from invoice");
+                while(rs.next())
+                    System.out.println(rs.getInt(1)+"  "+rs.getString(3));
+                con.close();
+            }catch(Exception e){ System.out.println(e);}
+
+    }
+
+    /*public static void insertInvoice(Date date, String description, double value, Boolean paid){
+
+    }
+    public static void updateInvoice(int id, Date date, String description, double value, Boolean paid){
+
+    }
+    public static void deleteInvoice(int id){
+
+    }
+*/
+
+
+
 }
